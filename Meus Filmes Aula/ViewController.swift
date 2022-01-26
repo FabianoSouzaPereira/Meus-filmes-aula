b3182a02182847445acd9ffa2ea087e0ae1d69f4
Fabiano <fabiano.pereira@seventh.com.br>
2022-01-26 20:06:13 -0300

    Initial Commit

---
+++ Meus Filmes Aula/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Meus Filmes Aula
+//
+//  Created by Fabiano De Souza Pereira on 26/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
