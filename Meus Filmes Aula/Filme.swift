af46c7b02e835953cf3b1ede313c050ef6a4823e
Fabiano <fabiano.pereira@seventh.com.br>
2022-03-13 11:31:30 -0300

    commit new

---
+++ Meus Filmes Aula/Filme.swift
@@ -0,0 +1,24 @@
+//
+//  Filme.swift
+//  Meus Filmes Aula
+//
+//  Created by Fabiano De Souza Pereira on 26/01/22.
+//
+
+import UIKit
+
+class Filme{
+    
+    var title: String!
+    var description: String!
+    var image: UIImage!
+    
+    init(title: String, description: String, image: UIImage){
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+}
+    
+    
+
